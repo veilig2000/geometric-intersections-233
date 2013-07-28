c1e19d99402e4ae0fce93a312342cf9896edb5d3
Jamie Kahgee <jamie.kahgee@gmail.com>
2013-07-28 16:02:38 -0400

    add missing documentation

--- Polygon.php
+++ Polygon.php
@@ -2,18 +2,45 @@
 
 class Polygon
 {
+    /**
+     * Points Collection
+     * @var Point_Collection
+     * @access private
+     */
     private $_points;
 
+    /**
+     * Constructor
+     *
+     * @param Point_Collection $points
+     *
+     * @return void
+     * @access public
+     */
     public function __construct(Point_Collection $points)
     {
         $this->_points = $points;
     }
 
+    /**
+     * Get the polygon points
+     *
+     * @return Point_Collection
+     * @access public
+     */
     public function getPoints()
     {
         return $this->_points;
     }
 
+    /**
+     * Add a point to the polygon
+     *
+     * @param Point $point
+     *
+     * @return $this
+     * @access public
+     */
     public function addPoint(Point $point)
     {
         $this->_points->add($point);
