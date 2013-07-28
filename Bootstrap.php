2cfa800a1789c0e7ace00ba548d9da571b465a4f
Jamie Kahgee <jamie.kahgee@gmail.com>
2013-07-28 14:58:50 -0400

    fix typo

--- Bootstrap.php
+++ Bootstrap.php
@@ -6,7 +6,7 @@ class Bootstrap
     public static function init()
     {
         $BASE_PATH = realpath(dirname(__FILE__));
-        set_include_path(get_include_path() . PATH_SEPARATOR . $BASE_PATH;
+        set_include_path(get_include_path() . PATH_SEPARATOR . $BASE_PATH);
         spl_autoload_register('Autoloader::load');
     }
 }
